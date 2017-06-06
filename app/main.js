f5183350bf284640c140fbc3b0bb886e450ba9c0
squirrel <914173855@qq.com>
2017-06-06 16:00:30 +0800

    dev

--- app/main.js
+++ app/main.js
@@ -12,6 +12,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import Greeter from './Greeter';
 
+
 import './css/main.css';//使用require导入css文件
 
 render(<Greeter />, document.getElementById('root'));
